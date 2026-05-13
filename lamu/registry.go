218c58bf0206d2f4af6ef002feb4b994b8d54043
Sandeep Kumar <sandeepqmar05@gmail.com>
2026-05-13 14:18:08 +0530

    reordering registry

--- lamu/registry.go
+++ lamu/registry.go
@@ -26,34 +26,34 @@ func MapSlice[T any, R any](slice []T, mapper func(T) R) []R {
 }
 
 func BuildAllRegistries(allPlugins []registry.Pair[string, Plugin]) {
-	FillRegistry(MapSlice(allPlugins, func(pair registry.Pair[string, Plugin]) func() PluginFeatures[CommandFactory] {
-		return pair.Value.CommandFactories
-	}), RegistryCommand)
-	FillRegistry(MapSlice(allPlugins, func(pair registry.Pair[string, Plugin]) func() PluginFeatures[Config] {
-		return pair.Value.Configs
-	}), RegistryConfig)
-	FillRegistry(MapSlice(allPlugins, func(pair registry.Pair[string, Plugin]) func() PluginFeatures[DBInitHook] {
-		return pair.Value.DBInitHooks
-	}), RegistryDBInit)
-	FillRegistry(MapSlice(allPlugins, func(pair registry.Pair[string, Plugin]) func() PluginFeatures[Generator] {
-		return pair.Value.Generators
-	}), RegistryGenerator)
-	FillRegistry(MapSlice(allPlugins, func(pair registry.Pair[string, Plugin]) func() PluginFeatures[views.GlobalLayer] {
-		return pair.Value.Layers
-	}), RegistryLayer)
 	FillRegistry(MapSlice(allPlugins, func(pair registry.Pair[string, Plugin]) func() PluginFeatures[UsefulFilesystem] {
 		return pair.Value.Migrations
 	}), RegistryMigrations)
+	FillRegistry(MapSlice(allPlugins, func(pair registry.Pair[string, Plugin]) func() PluginFeatures[DBInitHook] {
+		return pair.Value.DBInitHooks
+	}), RegistryDBInit)
+	FillRegistry(MapSlice(allPlugins, func(pair registry.Pair[string, Plugin]) func() PluginFeatures[Config] {
+		return pair.Value.Configs
+	}), RegistryConfig)
+	FillRegistry(MapSlice(allPlugins, func(pair registry.Pair[string, Plugin]) func() PluginFeatures[Generator] {
+		return pair.Value.Generators
+	}), RegistryGenerator)
+	FillRegistry(MapSlice(allPlugins, func(pair registry.Pair[string, Plugin]) func() PluginFeatures[CommandFactory] {
+		return pair.Value.CommandFactories
+	}), RegistryCommand)
 	FillRegistry(MapSlice(allPlugins, func(pair registry.Pair[string, Plugin]) func() PluginFeatures[any] {
 		return pair.Value.Models
 	}), RegistryModel)
+	FillRegistry(MapSlice(allPlugins, func(pair registry.Pair[string, Plugin]) func() PluginFeatures[views.GlobalLayer] {
+		return pair.Value.Layers
+	}), RegistryLayer)
 	FillRegistry(MapSlice(allPlugins, func(pair registry.Pair[string, Plugin]) func() PluginFeatures[components.PageInterface] {
 		return pair.Value.Pages
 	}), RegistryPage)
-	FillRegistry(MapSlice(allPlugins, func(pair registry.Pair[string, Plugin]) func() PluginFeatures[Route] {
-		return pair.Value.Routes
-	}), RegistryRoute)
 	FillRegistry(MapSlice(allPlugins, func(pair registry.Pair[string, Plugin]) func() PluginFeatures[*views.View] {
 		return pair.Value.Views
 	}), RegistryView)
+	FillRegistry(MapSlice(allPlugins, func(pair registry.Pair[string, Plugin]) func() PluginFeatures[Route] {
+		return pair.Value.Routes
+	}), RegistryRoute)
 }
--- plugins/p_otp/go.mod
+++ plugins/p_otp/go.mod
@@ -3,8 +3,8 @@ module github.com/UniquityVentures/lamu/plugins/p_otp
 go 1.26.1
 
 require (
-	github.com/UniquityVentures/lamu v0.4.3
-	github.com/UniquityVentures/lamu/plugins/p_users v0.4.3
+	github.com/UniquityVentures/lamu v0.4.4
+	github.com/UniquityVentures/lamu/plugins/p_users v0.4.4
 	gorm.io/gorm v1.31.1
 )
 
