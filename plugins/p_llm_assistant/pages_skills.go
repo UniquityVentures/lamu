11e01f2182cc9daff33b7e14f6e218fa9a2915cb
Sandeep Kumar <sandeepqmar05@gmail.com>
2026-07-05 18:01:18 +0530

    Skill export button fix

--- plugins/p_llm_assistant/pages_skills.go
+++ plugins/p_llm_assistant/pages_skills.go
@@ -124,7 +124,7 @@ func registerSkillsPages() {
 						Children: []components.PageInterface{
 							&components.FieldTitle{Getter: getters.Key[string]("$in.Name")},
 							&components.FieldSubtitle{Getter: getters.Key[string]("$in.Description")},
-							&components.ButtonLink{
+							&components.ButtonDownload{
 								Label: "Export Skill",
 								Icon:  "arrow-down-tray",
 								Link: lamu.RoutePath("llm_assistant.SkillsExportRoute", map[string]getters.Getter[any]{
